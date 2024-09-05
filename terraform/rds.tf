65fa4c49a125707fe361ab753104ca2092047af4
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2024-09-05 07:08:56 -0400

    update `comments` to the reference

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -4,8 +4,8 @@ resource "aws_db_instance" "app_db" {
   engine                 = "postgres"
   engine_version         = "13.11"
   instance_class         = "db.t3.micro"
-  username               = "dbadmin"
-  password               = "supersecurepassword"
+  username               = "dbadmin"             # Reference only and should be secured in terraform workspace env.
+  password               = "supersecurepassword" # Reference only and should be secured in terraform workspace env.
   db_subnet_group_name   = aws_db_subnet_group.app_db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
   multi_az               = true
