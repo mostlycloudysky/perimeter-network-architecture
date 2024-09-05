f0ded29b0233be5201e9350f78d24eb03b940d5b
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2024-09-04 22:35:29 -0400

    bump up `v6`

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-import models
+import models, schemas
 
 app = FastAPI()
 
@@ -14,15 +14,16 @@ def get_db():
         db.close()
 
 
-@app.post("/items/", response_model=models.Item)
-def create_item(item: models.Item, db: Session = Depends(get_db)):
-    db.add(item)
+@app.post("/items/", response_model=schemas.Item)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    db_item = models.Item(name=item.name, description=item.description)
+    db.add(db_item)
     db.commit()
-    db.refresh(item)
-    return item
+    db.refresh(db_item)
+    return db_item
 
 
-@app.get("/items/{item_id}", response_model=models.Item)
+@app.get("/items/{item_id}", response_model=schemas.Item)
 def read_item(item_id: int, db: Session = Depends(get_db)):
     item = db.query(models.Item).filter(models.Item.id == item_id).first()
     if item is None:
@@ -30,8 +31,8 @@ def read_item(item_id: int, db: Session = Depends(get_db)):
     return item
 
 
-@app.put("/items/{item_id}", response_model=models.Item)
-def update_item(item_id: int, item: models.Item, db: Session = Depends(get_db)):
+@app.put("/items/{item_id}", response_model=schemas.Item)
+def update_item(item_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
     db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
     if db_item is None:
         raise HTTPException(status_code=404, detail="Item not found")
@@ -42,7 +43,7 @@ def update_item(item_id: int, item: models.Item, db: Session = Depends(get_db)):
     return db_item
 
 
-@app.delete("/items/{item_id}", response_model=models.Item)
+@app.delete("/items/{item_id}", response_model=schemas.Item)
 def delete_item(item_id: int, db: Session = Depends(get_db)):
     item = db.query(models.Item).filter(models.Item.id == item_id).first()
     if item is None:
